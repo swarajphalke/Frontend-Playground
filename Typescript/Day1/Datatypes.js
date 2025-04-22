760106cb93c3549378bbc5e30f26aee1ad3aa634
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-22 21:57:38 +0530

    updating datatypes

--- Typescript/Day1/Datatypes.js
+++ Typescript/Day1/Datatypes.js
@@ -1,2 +1,4 @@
 var age = 25;
 console.log(age);
+var person = "John Doe";
+console.log(person);
--- Typescript/Day1/Datatypes.ts
+++ Typescript/Day1/Datatypes.ts
@@ -1,2 +1,5 @@
 let age : number = 25;
 console.log(age);
+
+let person : string = "John Doe";
+console.log(person);
