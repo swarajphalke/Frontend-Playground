b17caec355ad646a6616c9ec9b32bec769d9be48
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-24 11:46:27 +0530

    Optional Parameter Function in TypeScript

--- Typescript/Day2/Functions.ts
+++ Typescript/Day2/Functions.ts
@@ -1,5 +1,5 @@
 // Types of Functions in TypeScript
 // 1. Optional Parameter Function 
-// 1. Default Parameter Function 
-// 1. REST Parameter Function 
-// 1. Arrow Function 
+// 2. Default Parameter Function 
+// 3. REST Parameter Function 
+// 4. Arrow Function 
