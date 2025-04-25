428c0ed2e82c241167455ca5b55c57933bd44ab4
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-26 02:15:08 +0530

    Extending Interfaces in ts

---
+++ Typescript/Interface/ExtendingInterfaces.js
@@ -0,0 +1,6 @@
+var emp = {
+    name: "Swaraj",
+    age: 20,
+    ManagerId: 101,
+};
+console.log(emp);
---
+++ Typescript/Interface/ExtendingInterfaces.ts
@@ -0,0 +1,14 @@
+interface Men {
+  name: string;
+  age: number;
+}
+interface Manager extends Men {
+  ManagerId: number;
+}
+const emp: Manager = {
+  name: "Swaraj",
+  age: 20,
+  ManagerId: 101,
+};
+
+console.log(emp);
