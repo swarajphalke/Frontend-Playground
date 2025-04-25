09586fc80d275ee11394eca5fada5f89e9f4ed3a
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-26 02:12:29 +0530

    Functions in TypeScript

---
+++ Typescript/Functions/Functions.js
@@ -0,0 +1,38 @@
+"use strict";
+// Types of Functions in TypeScript
+// 1. Optional Parameter Function
+// 2. Default Parameter Function
+// 3. REST Parameter Function
+// 4. Arrow Function
+// Optional Parameter Function
+function disp(id, name, email) {
+    console.log("ID: " + id + " " + "Name: " + name);
+    if (email) {
+        console.log("Email: " + email);
+    }
+}
+disp(1, "Swaraj");
+console.log("-----------------");
+disp(1, "Swaraj", "swaraj@gmail.com");
+// Default Parameter Function
+function data(name, state = "Maharashtra") {
+    console.log("Name: " + name + " " + "State: " + state);
+}
+data("John");
+data("Alice", "Madhya Pradesh");
+// REST Parameter Function
+function addNo(...n) {
+    let i;
+    let sum = 0;
+    for (i = 0; i < n.length; i++) {
+        sum = sum + n[i];
+    }
+    return sum;
+}
+console.log("Sum of numbers: " + addNo(1, 2, 3, 4, 5));
+console.log("Sum of numbers: " + addNo(10, 20, 30, 40, 50, 60));
+// Arrow Function
+let show = (name) => {
+    console.log("Hello " + name);
+};
+show("Swaraj");
