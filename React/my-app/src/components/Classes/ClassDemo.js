bb2fb1a3ea811fea6bb7e3a1795f05d2112b8588
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-30 02:02:31 +0530

    Class Component in React

---
+++ React/my-app/src/components/Classes/ClassDemo.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <h2>
+        <u>Class Component Demo</u>
+      </h2>
+    );
+  }
+}
+
+export default Welcome;
