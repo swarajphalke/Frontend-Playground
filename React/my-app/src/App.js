7ec7804c434eee979b31be86cd53a1f88c315df4
Swaraj Phalke <swarajphalke555@gmail.com>
2025-04-30 02:02:05 +0530

    App.js in React

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,28 +1,17 @@
 import "./App.css";
-import Greet from "./components/Greet";
-import ClassEx from "./components/ClassEx";
-import ClassLife from "./components/ClassLife";
-import StateEx from "./components/StateEx";
-import PropEx from "./components/PropEx";
-import JsxEx from "./components/JsxEx";
-import JsxAttribute from "./components/JsxAttribute";
-import JsxFun from "./components/JsxFun";
+import ClassDemo from "./Components/Classes/ClassDemo";
+import ClassLife from "./Components/Classes/ClassLife";
+import FunDemo from './Components/Functions/FunDemo'
+import StateEx from './Components/Classes/StateEx'
+import StateEx1 from "./Components/Classes/StateEx1";
 function App() {
-  const data = { name: "PROFOUND" };
   return (
     <div className="App">
-      <header className="App-header">
-
-        <JsxFun />
-        <JsxAttribute />
-        <Greet />
-        <ClassEx />
-        <ClassLife />
-        <StateEx />
-        <PropEx val={data} />
-        <JsxEx />
-         
-      </header>
+      <ClassDemo />
+      <ClassLife/>
+      <StateEx/>
+      <StateEx1/>
+      <FunDemo />
     </div>
   );
 }
