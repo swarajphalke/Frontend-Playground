bacb2234790eda6768484cc13241bacba105aa3b
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-01 22:42:00 +0530

    useState in React

---
+++ ReactJS/hooks/src/Components/UseState/UseState.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+export default function Hook1() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <h1>States in React</h1>
+    <hr />
+    <h2>useState()</h2>
+      <p>Count : {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      &nbsp;
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+      &nbsp;
+      <button
+        onClick={() => {
+          setCount(count * 2);
+        }}
+      >
+        Multiple
+      </button>
+      &nbsp;
+      <hr />
+      <hr />
+    </>
+  );
+}
