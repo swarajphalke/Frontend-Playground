5c5ff61fadc3ffc76d2b49c8473efb4d77f800fc
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-02 21:07:04 +0530

    useContext hook in react

--- ReactJS/hooks/src/Components/UseContext/Home.js
+++ ReactJS/hooks/src/Components/UseContext/Home.js
@@ -1,8 +1,14 @@
+import { useContext } from "react";
+import { BioContext } from "./UseContext";
+
 export const Home = () => {
+  const { myName, myAge } = useContext(BioContext);
   return (
     <>
-    <h2>UseContext()1</h2>
-      <p>Hello Context API</p>
+      <h2>UseContext()1</h2>
+      <p>
+        Hello Context API. My name is {myName}. I am {myAge} years old.{" "}
+      </p>
     </>
   );
 };
--- ReactJS/hooks/src/Components/UseContext/UseContext.js
+++ ReactJS/hooks/src/Components/UseContext/UseContext.js
@@ -5,11 +5,11 @@ import {createContext, useContext} from 'react'
 export const BioContext = createContext()
 
 export const BioProvider = ({children}) =>{
-    const myName = 'vinod';
+    const myName = 'swaraj';
     const myAge = 20;
 
     return (
-        <BioContext.Provider value={myName}>
+        <BioContext.Provider value={{myName,myAge}}>
             {children}
         </BioContext.Provider>
     )
