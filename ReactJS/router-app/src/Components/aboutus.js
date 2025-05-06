b7ff1e5aedaecc0cfb52765de28711c0f9a0ea0f
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-07 01:41:12 +0530

    testing

--- ReactJS/router-app/src/Components/aboutus.js
+++ ReactJS/router-app/src/Components/aboutus.js
@@ -1,7 +1,6 @@
 export default function Aboutus(){
     return(
-        <>
-            <h2>About Page</h2>
+        <>            <h2>About Page</h2>
             <p>Learn more on this page</p>
         </>
     )
