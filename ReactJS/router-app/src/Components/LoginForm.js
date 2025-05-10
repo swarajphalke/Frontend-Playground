360d27601ba164a16607c312574217d995a741b4
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-10 20:10:46 +0530

    updated

--- ReactJS/router-app/src/Components/LoginForm.js
+++ ReactJS/router-app/src/Components/LoginForm.js
@@ -18,7 +18,6 @@ export default function LoginForm() {
     validate: (values) => {
       let errors = {};
       
-
       if (!values.email) {
         errors.email = "Email is required";
       } else if (!/\S+@\S+\.\S+/.test(values.email)) {
