7100f27f83b1fc3046dc399a1012a4a8a8e06a27
Swaraj Phalke <swarajphalke555@gmail.com>
2025-05-29 00:31:08 +0530

    updated

--- ReactJS/hooks/src/Components/UseReducer/index.js
+++ ReactJS/hooks/src/Components/UseReducer/index.js
@@ -4,7 +4,6 @@ export const ReducerComp = () => {
   const reducer = (state, action) => {
     if (action.type === "INCREMENT") {
       return state + 1;
-      
     }
     if (action.type === "DECREMENT") {
       return state - 1;
